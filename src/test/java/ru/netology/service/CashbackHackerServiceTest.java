27b880a67e71b81288099c6d1ee3f7e5ecd897df
Yuliya Gerashchenko <merg@list.ru>
2019-12-12 22:21:38 +0300

    Add Parametrized Test

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -2,25 +2,36 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackerServiceTest {
 
-    @Test
-    void calculateCashback() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 100;
-        Assertions.assertEquals(expected, actual);
+//    @Test
+//    void calculateCashback() {
+//        CashbackHackService cashbackHackService = new CashbackHackService();
+//        int amount = 900;
+//        int actual = cashbackHackService.remain(amount);
+//        int expected = 100;
+//        Assertions.assertEquals(expected, actual);
+//    }
+//    @Test
+//    void noCalculateCashback() {
+//        CashbackHackService cashbackHackService = new CashbackHackService();
+//        int amount = 1000;
+//        int actual = cashbackHackService.remain(amount);
+//        int expected = 0;
+//        Assertions.assertEquals(expected, actual);
+//    }
+
+        @ParameterizedTest
+        @CsvFileSource(resources="/CashbackDate.csv", numLinesToSkip=1)
+        void calculateCashback (int amount, int expected) {
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int actual = cashbackHackService.remain(amount);
+            Assertions.assertEquals(expected, actual);
+        }
     }
-    @Test
-    void noCalculateCashback() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 0;
-        Assertions.assertEquals(expected, actual);
-    }
-}
+
